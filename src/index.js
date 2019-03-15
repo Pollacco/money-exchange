3617db0560126c7cf4e555f71bb99379a2079e60
Andrey <andrey.sviontek@gmail.com>
2019-03-15 11:14:50 +0500

    fix: complete 100% task

--- src/index.js
+++ src/index.js
@@ -9,25 +9,26 @@ module.exports = function makeExchange(currency) {
     let p = 1;
     let coins = {};
     if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"}
-    for (i = 1; currency >= h; i++) {
-        currency -= h;  
-        coins.H = i;      
-    }        
-    for (i = 1; currency >= q; i++) {
-        currency -= q;  
-        coins.Q = i;      
+    
+    if (currency >= h) {
+       coins.H =  Math.floor(currency / h);
+       currency = currency % h;
     }
-    for (i = 1; currency >= d; i++) {
-        currency -= d;  
-        coins.D = i;      
+    if (currency >= q) {
+       coins.Q =  Math.floor(currency / q);
+       currency = currency % q;
     }
-    for (i = 1; currency >= n; i++) {
-        currency -= n;  
-        coins.P = n;      
+    if (currency >= d) {
+        coins.D =  Math.floor(currency / d);
+        currency = currency % d;
     }
-    for (i = 1; currency >= p; i++) {
-        currency -= p;  
-        coins.P = i;      
+    if (currency >= n) {
+        coins.N =  Math.floor(currency / n);
+        currency = currency % n;
+    }
+    if (currency >= p) {
+        coins.P =  Math.floor(currency / p);
+        currency = currency % p;
     }
     return coins;
 }
